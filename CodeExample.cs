29aaebe496c5ff163121e2955614b941baacc795
Amir0405 <amirbivas@gmail.com>
2016-06-14 21:54:16 +0000

    Add files via upload
    Code Example - C# - Printing numbers 1 to 100, replacing multipliers of 3 with "Bud",  multipliers of 5 with "Soft" and multipliers of both 3 and 5 with "BudSoft".

---
+++ CodeExample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1B
+{
+    class CodeExample
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.Write(" BudSoft ");
+                    }
+                    else
+                    {
+                        Console.Write(" Bud ");
+                    }
+                }
+                if (i % 5 == 0 && i % 3 != 0)
+                {
+                    Console.Write(" Soft ");
+                }
+                else if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.Write($" {i} ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
